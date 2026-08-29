8f219313687818c36a501880a269b06e95fbd578
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2026-08-29 08:40:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function App() {
+  return (
+    <main className="welcome-page">
+      <h1>Welcome to my React app</h1>
+      <p>By 2400030347</p>
+    </main>
+  )
+}
+
+export default App
